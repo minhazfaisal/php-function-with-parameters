4ede225436e18d8eb986191a0dfca1a19992e8fe
Minhaz Faisal <faisalahmed01944@gmail.com>
2026-07-28 22:51:49 +0600

    Update index.php

--- index.php
+++ index.php
@@ -55,6 +55,16 @@
             echo "<h4> Hello, " . $name2 . "!</h4>";
         }
         grettings6("Minhaz");
+
+        function greetings7($name){
+            echo "<h4> Hello $name </h4>";
+        }
+        greetings7("Minhaz");
+
+        $greetings8 = function($name = "world"){
+            echo "<h4> Hello $name </h4>";
+        };
+        $greetings8();
         
         ?>
     </div>
